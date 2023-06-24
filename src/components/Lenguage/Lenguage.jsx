52cc1d82afd45ad6b7ae88e34a78d0ec9aadb96c
Santiago Quintero <santiagoquinteroperfil@gmail.com>
2023-06-23 21:03:49 -0500

    Se crea prímera sección de landing, falta empezar con lógica

--- src/components/Lenguage/Lenguage.jsx
+++ src/components/Lenguage/Lenguage.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { WorldSvg } from '../WolrdSvg/WorldSvg'
 
-function Lenguage () {
+function Lenguage ({ hideLenguage, clickOption, setClickOption }) {
+  const isClickOption = (event) => {
+    event.stopPropagation()
+    if (hideLenguage === false) setClickOption(true)
+  }
+
+  if (!hideLenguage && clickOption) {
+    window.addEventListener('click', (event) => {
+      event.stopPropagation()
+      setClickOption(false)
+    })
+  }
   return (
     <div className='flex items-center justify-center gap-2 bg-black/30 border border-gray-500 py-1 px-2'>
       <WorldSvg />
-      <select className=' bg-transparent text-gray-100 font-netflix-medium custom-select  '>
-        <option value='english' className='bg-gray-800/60 '>
-          English
+      <select
+        className={ ' w-auto bg-transparent text-gray-100 font-netflix-medium custom-select' }
+        onClick={isClickOption}
+      >
+        <option value='english' className='bg-gray-800/60 w-[50px]  '>
+          {hideLenguage || clickOption ? 'English' : ''}
         </option>
-        <option value='español'>Español</option>
+        <option value='español' className='bg-gray-800/60  '>
+          {hideLenguage || clickOption ? 'Español' : ''}
+        </option>
       </select>
     </div>
   )
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,44 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Main } from '../../components/Main/Main'
 import { Logo } from '../../components/Logo/Logo'
 import { Lenguage } from '../../components/Lenguage/Lenguage'
 import './Landing.css'
 
 function Landing () {
-  const backGroundImage =
-    'https://assets.nflxext.com/ffe/siteui/vlv3/530fc327-2ddb-4038-a3f0-2da2d9ccede1/6df01d4b-de02-4e92-9faa-159d4d5fa06d/CO-es-20230619-popsignuptwoweeks-perspective_alpha_website_small.jpg'
+  const [hideLenguage, setHideLenguage] = useState(false)
+  const [clickOption, setClickOption] = useState(false)
 
+  const innerWidth = () => {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 600) {
+        setHideLenguage(true)
+        setClickOption(true)
+      } else {
+        setHideLenguage(false)
+        setClickOption(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    innerWidth()
+  }, [hideLenguage])
   return (
-    <>
-      <div className=' min-h-[100vh] w-full absolute z-10'>
-        <header className='flex w-full justify-between py-7 px-8 '>
-          <Logo />
-          <div className='flex gap-3'>
-            <Lenguage />
-            <button className='bg-[rgb(193,17,25)] text-gray-100 font-netflix-bold font-bold text-[10px] px-3 py-2 rounded-md'>
-              Iniciar sesión
-            </button>
-          </div>
-        </header>
-        <Main>
-          <section className='w-full font-netflix-medium  text-gray-100 flex flex-col justify-center items-center px-6'>
-            <h1 className='font-netflix-bold font-bold text-[28px] text-center mt-10'>
-              Películas, series ilimitadas y mucho más
-            </h1>
-            <p className='mt-4 text-center'>
-              Disfruta donde quieras. Cancela cuando quieras.
-            </p>
-            <p className='mt-4 text-center w-[90%]'>
-              ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o
-              reiniciar tu membresía de Netflix.
-            </p>
+    <div className={'background'}>
+      <div className='gradient '></div>
+      <header className='flex w-full justify-between py-7 px-8 absolute '>
+        <Logo />
+        <div className='flex gap-3'>
+          <Lenguage
+            hideLenguage={hideLenguage}
+            clickOption={clickOption}
+            setClickOption={setClickOption}
+          />
+          <button className='bg-[rgb(193,17,25)] text-gray-100 font-netflix-bold font-bold text-[10px] px-3 py-2 rounded-md'>
+            Iniciar sesión
+          </button>
+        </div>
+      </header>
+      <Main>
+        <section className='w-full font-netflix-medium  text-gray-100 flex flex-col justify-center items-center px-[33px] absolute mt-[95px]'>
+          <h1 className='font-netflix-bold font-bold text-[30px] text-center mt-10 w-[99%]'>
+            Películas, series ilimitadas y mucho más
+          </h1>
+          <p className='mt-4 text-center w-full text-lg'>
+            Disfruta donde quieras. Cancela cuando quieras.
+          </p>
+          <p className='mt-4 text-center w-[90%]'>
+            ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o
+            reiniciar tu membresía de Netflix.
+          </p>
+          <div className=' flex flex-col sm:flex-row w-4/5 sm:flex sm:gap-4 sm:justify-between justify-center items-center'>
             <input
               type='email'
               placeholder='Email'
-              className='mt-5 w-5/6 h-10 px-3 bg-black/60 border border-gray-400 focus:border-2 focus:border-gray-300 focus:outline-none focus:placeholder:text-[10px] focus:flex focus:items-start   '
+              className='mt-5 w-5/6 h-[45px] px-3 bg-black/60 border border-gray-400 focus:border-2 focus:border-gray-300 focus:outline-none focus:placeholder:text-[10px] focus:flex focus:items-start   '
               autoComplete='email'
             />
-            <button className=' flex text-gray-100 p-3 bg-[rgb(193,17,25)] rounded-md mt-4 font-netflix-bold font-bold'>
+            <button className=' h-[45px] w-[130px] text-center justify-between flex text-gray-100  bg-[rgb(193,17,25)] rounded-md mt-4 font-netflix-bold font-bold items-center px-2'>
               Comenzar
               <svg
                 width='24'
@@ -57,23 +77,10 @@ function Landing () {
                 ></path>
               </svg>
             </button>
-          </section>
-        </Main>
-      </div>
-      <div className='relative z-0 '>
-        <div className=' h-full  w-full'>
-          <div className=' z-0 min-h-[100vh] object-cover'>
-            <img
-              alt=''
-              aria-hidden='true'
-              src={backGroundImage}
-              className='h-[70vh] object-cover object-left '
-            />
           </div>
-          <div className='gradient '></div>
-        </div>
-      </div>
-    </>
+        </section>
+      </Main>
+    </div>
   )
 }
 
